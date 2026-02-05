a908b4eefbeeb9bc3c177aaa944be9c5dfbd9fa7
chae2 <saintcharry22@gmail.com>
2026-02-05 23:11:19 +0900

    60 - Provider

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -32,21 +33,36 @@ public class SingletonWithPrototypeTest1 {
 
     ClientBean client2 = ac.getBean(ClientBean.class); // 하나만 생성될 것. 싱글턴이라.
     int count2 = client2.logic();
-    assertEquals(count2, 2);
+    assertEquals(count2, 1);
   }
 
   @Scope("singleton")
   static class ClientBean {
-    private final PrototypeBean prototypeBean; // 생성시점에 주입
 
+    @Autowired
+    private Provider<PrototypeBean> prototypeBeanProvider; // 2. 자바 Provider 사용
+//    private ObjectProvider<PrototypeBean> prototypeBeanProvider; // 1. 스프링 ObjectProvider 사용
+
+
+    // 0. 초기 문제재현 코드
+    /*private final PrototypeBean prototypeBean; // 생성시점에 주입
     @Autowired
     public ClientBean(PrototypeBean prototypeBean) {
       this.prototypeBean = prototypeBean;
-    }
+    }*/
 
     public int logic() {
-      prototypeBean.addCount();
-      return prototypeBean.getCount();
+      // 2. 자바 Provider 사용
+      PrototypeBean pt = prototypeBeanProvider.get();
+
+      // 1. 스프링 ObjectProvider 사용
+//      PrototypeBean pt = prototypeBeanProvider.getObject(); // DL; 스프링 컨테이너 내에 해당 빈을 찾아 반환함.
+      pt.addCount();
+      return pt.getCount();
+
+      // 0. 초기 문제재현 코드
+      /*prototypeBean.addCount();
+      return prototypeBean.getCount();*/
     }
   }
 
