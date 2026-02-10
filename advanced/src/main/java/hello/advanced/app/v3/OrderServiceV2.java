5a0ed78b3e6c0819f755be5ad1d2e75ca2b0558e
chae2 <saintcharry22@gmail.com>
2026-02-10 20:57:23 +0900

    13-14. 필드 동기화 - 애플리케이션 적용

---
+++ advanced/src/main/java/hello/advanced/app/v3/OrderServiceV2.java
@@ -0,0 +1,29 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.helloTrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null; // try 안에 넣을 수 없음..
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            // 핵심 로직
+            orderRepository.save(status.getTraceId(), itemId);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 로그 추적기 요구사항 - 서비스에 영향을 줘선 안된다: 예외를 꼭 다시 던져줘야 함.
+        }
+    }
+}
