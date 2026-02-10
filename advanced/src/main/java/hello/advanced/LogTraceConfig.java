8c4af0c81e1469fb040da02a4d9c1fe1f1743f75
Chaeil Moon <102636528+chae2@users.noreply.github.com>
2026-02-10 22:51:44 +0900

    [Section 3. ThreadLocal] (#7)
    * 4. build settings
    
    * 5-6. 예제 프로젝트 만들기
    
    * 7. log tracker
    
    * 8. log tracker V1에 적용
    
    * 9. log tracker `HelloTraceV2` dev & test
    
    * 10. `HelloTraceV2` application 적용
    
    * 12. 필드 동기화 - 로그추적기 개발
    
    * 13-14. 필드 동기화 - 애플리케이션 적용
    
    * 15. 동시성문제 예제
    
    * 16-17. ThreadLocal 예제
    
    * fix: 패키지 실수
    
    * 18. ThreadLocal 로그추적기 개발
    
    * 19. ThreadLocal 로그추적기 애플리케이션 적용

---
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package hello.advanced;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+    @Bean
+    public LogTrace logTrace() {
+//        return new FieldLogTrace(); // [필드 동기화] 버전 - 싱글톤으로 로그추적기 등록.
+        return new ThreadLocalLogTrace(); // 동시성 문제 해결 [ThreadLocal] 버전.
+    }
+}
---
+++ advanced/src/main/java/hello/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null; // try 안에 넣을 수 없음..
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            // 핵심 로직
+            orderRepository.save(itemId);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 로그 추적기 요구사항 - 서비스에 영향을 줘선 안된다: 예외를 꼭 다시 던져줘야 함.
+        }
+    }
+}
