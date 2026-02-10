a64fe32f31ce5c8f422ea85f4fb3ec8ea3fc10ef
chae2 <saintcharry22@gmail.com>
2026-02-10 22:10:49 +0900

    19. ThreadLocal 로그추적기 애플리케이션 적용

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class LogTraceConfig {
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace(); // 싱글톤으로 로그추적기 등록.
+//        return new FieldLogTrace(); // [필드 동기화] 버전 - 싱글톤으로 로그추적기 등록.
+        return new ThreadLocalLogTrace(); // 동시성 문제 해결 [ThreadLocal] 버전.
     }
 }
